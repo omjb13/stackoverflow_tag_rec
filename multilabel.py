5f70a46c156617ff3dfd15db5e84af85ae4c95e1
Om Bathija <2487338+omjb13@users.noreply.github.com>
2017-12-14 12:26:46 -0600

    Added F1, recall and precision measurement

--- multilabel.py
+++ multilabel.py
@@ -5,6 +5,8 @@ from sys import argv
 from sklearn.model_selection import train_test_split
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
+from sklearn.metrics import f1_score, recall_score, precision_score
+import random
 
 from helpers import get_tf_idf_matrices, create_multilabel_label_vector
 
@@ -27,6 +29,7 @@ def main():
 
     # FEATURE EXTRACTION 
     print "Performing TF-IDF..."
+
     features, scores = get_tf_idf_matrices(title_and_body)
     # we need to save the binarizer so we can convert back to a list of 
     # labels later for the error checking. 
@@ -40,34 +43,22 @@ def main():
     print "X Train and test : ", x_train.shape, x_test.shape
     print "y Train and test : ", y_train.shape, y_test.shape
 
+    # MODEL TRAINING
+    print "Training model now..."
     clf = OneVsRestClassifier(LinearSVC(random_state=0)).fit(x_train, y_train)
     predicted_labels = clf.predict(x_test)
 
-    # convert back to lists of labels
-    predicted_labels = binarizer.inverse_transform(predicted_labels)
-    y_test = binarizer.inverse_transform(y_test)
+    # Using the micro F1 score because we can have labels that end up with an F1
+    # score of 0 as there are no samples for that label, thereby bringing down overall F1
+    # Micro circumvents this by looking at global false/true positive/negatives as opposed
+    # to per-class
+    f1 = f1_score(y_test, predicted_labels, average="micro")
+    recall = recall_score(y_test, predicted_labels, average="micro")
+    precision = precision_score(y_test, predicted_labels, average="micro")
 
-    error = 0
-    error_checking = zip(predicted_labels, y_test)
-
-    # this bit is hacky, it's just to get the original question text back
-    offset = int(math.floor(len(data) * (1 - test_size)))
-
-    print "Beginning error checking..."
-
-    for i, (p, t) in enumerate(error_checking):
-        p = set(p)
-        t = set(t)
-        # do any of the predicted labels overlap with the true labels? 
-        # if so, not an error. 
-        if not p.intersection(t):
-            print "predicted : ", p
-            print "true : ", t
-            current_record = data[offset + i]
-            print "entire record : \n", current_record
-            error += 1
-            print ""
-    print "%d errors out of %d samples" % (error, len(y_test))
+    print "Recall score is %f" % (recall)
+    print "Precision score is %f" % (precision)
+    print "F1 score is %f" % (f1)
 
 
 if __name__ == '__main__':
